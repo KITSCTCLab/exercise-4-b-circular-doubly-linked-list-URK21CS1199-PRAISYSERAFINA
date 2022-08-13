1e40d1f15de5dba11bb6da7fa58c8205f66e28a3
URK21CS1199-PRAISYSERAFINA <109951858+URK21CS1199-PRAISYSERAFINA@users.noreply.github.com>
2022-08-13 21:37:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ while len(actual_list) < length_of_circular_linked_list and index < len(circular
     element = circular_linked_list[index]
     if element not in actual_list:
         actual_list.append(element)
-    index = 1
+    index += 1
 
 print(len(actual_list))
 print(" ".join(str(num) for num in actual_list))
